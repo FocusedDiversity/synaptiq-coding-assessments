f292e6623ae2392534b79655cc9af970d3c786cd
Aid Idrizović <idrizovicaid@gmail.com>
2023-10-18 05:22:46 -0500

    complete project set up, project structure, pages, base components & other scaffolding

---
+++ react/pages/singledate.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SharedLayout from '../app/SharedLayout';
+
+const SingleDate: React.FC = () => {
+    return (
+        <SharedLayout>
+            <h1>Single Date Example</h1>
+        </SharedLayout>
+    );
+};
+
+export default SingleDate;
