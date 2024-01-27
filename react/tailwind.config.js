667fc55b7e5b304b37d7f0cc472ec31177a82701
Andrew Kowalczyk <andrewrkowalczyk@gmail.com>
2024-01-27 06:06:35 -0800

    add dark mode to tailwind config

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./stories/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: ["class", "[data-mode='dark']"],
   theme: {
     extend: {
       backgroundImage: {
